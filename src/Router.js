6b188dbc149b56568e46aaea6169bcd7f59e7b17
Jan Strassburg <jan.strassburg@gmail.com>
2015-03-30 14:08:55 +0200

    gh

--- src/Router.js
+++ src/Router.js
@@ -34,8 +34,19 @@ App.Router = {
 				});
 			});
 
-			this.get('#/search', function () {
-				nav.currentView('search');
+			this.get('#/search/:term', function () {
+				var request = $.ajax({
+					url: "/tv-index/api/search/" + this.params['term'],
+					contentType: "application/json",
+					beforeSend: function (xhr) {
+						xhr.setRequestHeader("Auth", $.cookie("auth"));
+					}
+				});
+				request.done(function (response) {
+					App.ViewModel.SearchResults.shows.removeAll();
+					ko.utils.arrayPushAll(App.ViewModel.SearchResults.shows, response);
+					nav.currentView('search');
+				});
 			});
 
 		}).run(route);
--- src/ViewModel/Navigation.js
+++ src/ViewModel/Navigation.js
@@ -4,17 +4,6 @@ App.ViewModel.Navigation = {
 	searchTerm: ko.observable(),
 
 	search: function () {
-		var request = $.ajax({
-			url: "/tv-index/api/search/" + this.searchTerm(),
-			contentType: "application/json",
-			beforeSend: function (xhr) {
-				xhr.setRequestHeader("Auth", $.cookie("auth"));
-			}
-		});
-		request.done(function (response) {
-			App.ViewModel.SearchResults.shows.removeAll();
-			ko.utils.arrayPushAll(App.ViewModel.SearchResults.shows, response);
-			window.location.hash = '/search';
-		});
+		window.location.hash = '/search/'+this.searchTerm();
 	}
 };
